4cc383e2651833d4cedb35034c0d665c8c9d2595
Kiran Shrestha <kiranshrestha2069@gmail.com>
2025-03-31 15:39:17 -0500

    second commit

---
+++ demo.py
@@ -0,0 +1,15 @@
+# from src.logger import logging
+# from src.exception import MyException   
+# import sys
+
+# try:
+#     a = 1 + "2"
+# except Exception as e:
+#     logging.info(e)
+#     raise MyException(e, sys) from e
+
+from src.pipline.training_pipeline import TrainPipeline 
+
+pipeline = TrainPipeline()
+pipeline.run_pipeline()
+
